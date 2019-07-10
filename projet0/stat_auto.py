9563390817fbe80bead5f44e586d69b74060b358
broucedrick <52262846+broucedrick@users.noreply.github.com>
2019-07-10 09:39:21 +0000

    Add files via upload

--- projet0/stat_auto.py
+++ projet0/stat_auto.py
@@ -20,15 +20,11 @@ print (nb_page)
 plus_chere = 0
 nb_poste = 0	
 	
-hier = datetime.datetime.now() - datetime.timedelta(days=1)
+hier = (datetime.datetime.now() - datetime.timedelta(days=1)).date()
 def date_hier(date):
 	global jour
 
-	jour = ""
-
-	dt_obj = datetime.datetime.strptime(date, '%d.%m.%Y %H:%M')
-	
-	jour = dt_obj.date()
+	jour = datetime.datetime.strptime(date, '%d.%m.%Y %H:%M').date()
 		#print jour
 	return jour
 
@@ -54,7 +50,8 @@ while link_statut == 200:
 	page = BeautifulSoup(req.text, "lxml")
 	for poste in page.findAll({'article'}):
 			date = poste.find('time').get('datetime')
-			if date_hier(date) == hier.date():
+			type_vehicule = poste.find('span',{'class':'address'}).getText().strip()[0]
+			if date_hier(date) == hier and type_vehicule == 'V':
 				nb_poste += 1
 				prix = float(ast.literal_eval(poste.get('data-event'))['price'])
 				#print (prix)
@@ -67,16 +64,16 @@ while link_statut == 200:
 					moins_chere = int(prix)
 					marqe = marque
 	nb_page += 1
-	# print (nb_page)
+	print (nb_page)
 	lien = "https://deals.jumia.ci/abidjan/vehicules?page="+ str(nb_page)
 	#print (lien)
 	req = requests.get(lien)
 	link_statut = req.status_code
-	#print(nb_page, link_statut)
-	if nb_page == 30:
-		break
+	# print(nb_page, link_statut)
+	# if nb_page == 30:
+	# 	break
 
-print ('Hier : ',hier.date())
+print ('Hier : ',hier)
 print ('Il y a eu ', nb_poste,' voiture(s) postee(s) sur jumia Deals')
 print ('La voiture la plus chere etait une ',marq, 'elle vaut ',plus_chere)
 print ('La voiture la moins chere etait une ',marqe, 'elle vaut ',moins_chere)
